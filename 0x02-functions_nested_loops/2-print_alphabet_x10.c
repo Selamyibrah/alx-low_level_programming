3f3cd0f6a815c8860617f522c2ddd17ea197e099
selam <www.selamyibarh@gmail.com>
2023-04-25 04:44:56 -0700

    function, nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,13 +2,13 @@
 /**
  * print_alphabet_x10 - print the alphabet 10 times.
  *
- * Return: Always 0
+ * Return: Always 0 (success)
  */
 void print_alphabet_x10(void)
 {
 int i;
 int j;
-for (j = 1; i <= 10; i++)
+for (i = 1; i <= 10; i++)
 {
 for (j = 97; j <= 122; j++)
 {
