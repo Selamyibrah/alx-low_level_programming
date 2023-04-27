c5addeca7e2e950a9a95923e8d7257d133efeeaf
selam <www.selamyibarh@gmail.com>
2023-04-27 13:50:48 -0700

    loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,7 +9,7 @@ int main(void)
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i % 3 == 0 && i % 5 == 0)
+if ((i % 3 == 0) && (i % 5 == 0))
 {
 printf("FizzBuzz");
 }
@@ -27,7 +27,7 @@ printf("%d", i);
 }
 if (i != 100)
 {
-printf(" ")
+printf(" ");
 }
 }
 printf("\n");
