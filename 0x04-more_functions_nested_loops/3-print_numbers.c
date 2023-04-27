8cfedaef899781066de80234a0739c5fd07a68e7
selam <www.selamyibarh@gmail.com>
2023-04-27 13:27:58 -0700

    loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,11 +6,10 @@
  */
 void print_numbers(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
+int c;
+for (c = 0; c <= 9; c++)
 {
-if (!(c == '2' || c == '4'))
-_putchar(c);
+_putchar(c + '0');
 }
 _putchar('\n');
 }
